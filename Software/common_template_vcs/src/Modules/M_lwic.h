94880456203606f8617f089d8e54fee27a6cbf32
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2020-10-13 08:09:36 +0550

    Deployed adabd97 with MkDocs version: 1.1.2

--- Software/common_template_vcs/src/Modules/M_lwic.h
+++
@@ -1,9 +0,0 @@
-#ifndef _M_LWIC_H
-#define _M_LWIC_H
-#include "../template.h"
-#include "../Common/pinouts.h"
-
-void lwic_setup();
-void lwic_loop();
-
-#endif // !_M_LWIC_H
--- Software/common_template_vcs/src/template.h
+++
@@ -1,6 +0,0 @@
-#ifndef  _TEMPLATE_H
-#define  _TEMPLATE_H
-
-#define ENABLE_OTA true
-
-#endif // ! _TEMPLATE_H
